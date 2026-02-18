9f623f2ccd63f9e539c6f491ad1d005b9966dccd
Thomas Suessli <thommy.suessli@bluewin.ch>
2026-02-18 17:24:41 +0900

    Erstellung aller Models

---
+++ Stromabrechnungen/Models/Stromgemeinschaft.swift
@@ -0,0 +1,25 @@
+import Foundation
+import SwiftData
+import SwiftUI
+
+@Model
+final class Stromgemeinschaft {
+    var bezeichnung: String
+    var abrechnungskonto: String
+    var bild: Data?
+
+    @Relationship(deleteRule: .cascade, inverse: \Stromrechnung.stromgemeinschaft)
+    var stromrechnungen: [Stromrechnung] = []
+
+    @Relationship(deleteRule: .cascade, inverse: \Stromabrechnung.stromgemeinschaft)
+    var stromabrechnungen: [Stromabrechnung] = []
+
+    @Relationship(deleteRule: .cascade, inverse: \Bezugspartei.stromgemeinschaft)
+    var bezugsparteien: [Bezugspartei] = []
+
+    init(bezeichnung: String, abrechnungskonto: String, bild: Data? = nil) {
+        self.bezeichnung = bezeichnung
+        self.abrechnungskonto = abrechnungskonto
+        self.bild = bild
+    }
+}
